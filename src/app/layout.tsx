c5601f4fbe02e6c0671c1b4f9642df5d1f681c31
osemin <andrew041204@snu.ac.kr>
2026-02-10 02:44:57 +0900

    로고 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   description:
     "경제학과 컴퓨터공학을 공부하고 있습니다. 게임이론과 산업조직론에 관심이 있으며, 비정기적으로 포스트를 업로드할 예정입니다.",
   icons: {
-    icon: "/icon.png",
+    icon: "/favicon.ico",
   },
 };
 
