f0672c26c9dcc348dc5b5821e7171677f8fd848b
osemin <andrew041204@snu.ac.kr>
2026-02-10 02:38:47 +0900

    로고 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export const metadata: Metadata = {
   title: "Semin's Blog",
   description:
     "경제학과 컴퓨터공학을 공부하고 있습니다. 게임이론과 산업조직론에 관심이 있으며, 비정기적으로 포스트를 업로드할 예정입니다.",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
